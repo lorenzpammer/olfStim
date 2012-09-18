30ec22f8bab2893d77a8dbe4420f7be5fccad701
lorenzpammer <lorenzpammer@gmail.com>
2012-09-18 19:01:18 +0200

    Updated getUserNotes.m. When calling function to extract notes, it will clear the notes field. That way notes will be cleared after every trial. Closes #4

--- protocols/+protocolUtilities/getUserNotes.m
+++ protocols/+protocolUtilities/getUserNotes.m
@@ -1,6 +1,8 @@
 function notes = getUserNotes(h)
 % notes = getUserNotes(h) returns a string containing the text the user
-% wrote into the notes field of the gui.
+% wrote into the notes field of the gui and will clear the notes field
+% after it extracted the notes.
+%
 
 %% 
 if nargin < 1
@@ -10,6 +12,7 @@ end
 %% Get notes
 try
     notes = get(h.sessionNotes.notesFigureField,'String'); % Extract text in the notes field.
+    set(h.sessionNotes.notesFigureField,'String',''); % Delete text of previous trial
 catch
     notes = [];
 end
